70d534ae80ae226970c9621bb9cbe7ca401b460c
Bruno Leone A P Corrêa <gerencia@acetime.com.br>
2022-05-08 01:02:19 -0300

    Adicionando swagger

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API demo')
+    .setDescription('API to exercise')
+    .setVersion('1.0')
+    .addTag('mongodb')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
   console.log('port => ', process.env.PORT);
   await app.listen(process.env.PORT || 3000);
 }
